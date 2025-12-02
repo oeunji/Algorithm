cc0068696267b5f4f6ef492205047c7bd0d3d35c
oeunji <oieunxe@gmail.com>
2025-12-02 22:50:37 +0900

    두 수의 곱 구하기

--- Algorithm/Level0/두_수의_곱_구하기.swift
+++ Algorithm/Level0/두_수의_곱_구하기.swift
@@ -10,7 +10,3 @@ import Foundation
 func solution(_ num1:Int, _ num2:Int) -> Int {
     return num1 * num2
 }
-
-if let num1 = readLine(), let num2 = readLine() {
-    print(solution(Int(num1)!, Int(num2)!))
-}
