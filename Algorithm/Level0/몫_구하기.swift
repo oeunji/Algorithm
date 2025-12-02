0a5fe153763ce844b02c807fbdb1648aeb4d8146
oeunji <oieunxe@gmail.com>
2025-12-02 23:27:42 +0900

    251202

--- Algorithm/Level0/몫_구하기.swift
+++ Algorithm/Level0/몫_구하기.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 private func solution(_ num1:Int, _ num2:Int) -> Int {
-    return num1 + num2
+    return num1 / num2
 }
--- Algorithm/Level0/숫자_비교하기.swift
+++ Algorithm/Level0/숫자_비교하기.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 private func solution(_ num1:Int, _ num2:Int) -> Int {
-    return num1 + num2
+    if num1 == num2 {
+        return 1
+    }
+    else {
+        return -1
+    }
 }
