934e9ca1e7b396336f52d75bc1c09450b23e65f7
oeunji <oieunxe@gmail.com>
2025-12-02 22:56:58 +0900

    두 수의 차 구하기

--- Algorithm/Level0/main.swift
+++ Algorithm/Level0/main.swift
@@ -6,7 +6,3 @@
 //
 
 import Foundation
-
-private func solution(_ num1:Int, _ num2:Int) -> Int {
-    return num1 * num2
-}
--- Algorithm/Level0/두_수의_차_구하기.swift
+++ Algorithm/Level0/두_수의_차_구하기.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  두_수의_차_구하기.swift
 //  Algorithm
 //
 //  Created by 이은지 on 12/2/25.
@@ -8,5 +8,5 @@
 import Foundation
 
 private func solution(_ num1:Int, _ num2:Int) -> Int {
-    return num1 * num2
+    return num1 - num2
 }
